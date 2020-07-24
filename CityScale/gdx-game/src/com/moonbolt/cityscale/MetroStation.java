92d763c88ceef365d9417f8b02298b80c2c56e4b
Yoshiiro07 <59020911+Yoshiiro07@users.noreply.github.com>
2020-07-24 02:39:20 -0300

    Update
    Update

--- CityScale/gdx-game/src/com/moonbolt/cityscale/MetroStation.java
+++ CityScale/gdx-game/src/com/moonbolt/cityscale/MetroStation.java
@@ -4,9 +4,11 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector3;
@@ -21,10 +23,14 @@ public class MetroStation implements Screen, ApplicationListener, InputProcessor
 	private String[] config;
 	private String platform;
 	private Sprite spr_master;   
-			
-	private String frameState = "stop";
+	private Sprite spr_Background;
+	private Texture tex_Background;
 	
-	private int mov = 0;
+	private Sprite spr_metro;
+	private Texture tex_metro;
+	
+	//fonts
+	private BitmapFont font_master;
 	
 	//Camera
 	private OrthographicCamera camera;
@@ -42,6 +48,20 @@ public class MetroStation implements Screen, ApplicationListener, InputProcessor
 		viewport.apply();
 		camera.position.set(camera.viewportWidth/2,camera.viewportHeight/2,0);
 		Gdx.input.setInputProcessor(this);
+		
+		//font
+		font_master = new BitmapFont(Gdx.files.internal("data/font/impact.fnt"),Gdx.files.internal("data/font/impact.png"), false);
+		font_master.setColor(Color.RED);
+		font_master.getData().setScale(0.11f,0.23f);
+		font_master.setUseIntegerPositions(false);	
+		
+		//Sprites
+		tex_Background = new Texture(Gdx.files.internal("data/maps/metrostation.png"));
+		spr_Background = new Sprite(tex_Background);
+		spr_Background.setSize(100, 100);
+		
+		tex_metro = new Texture(Gdx.files.internal("data/assets/metro.png"));
+		spr_metro = new Sprite(tex_metro);
 	}
 	
 	
@@ -56,8 +76,9 @@ public class MetroStation implements Screen, ApplicationListener, InputProcessor
 	    
 		game.batch.begin();
 			
-		spr_master = gameControl.LoadElements("light1");
-		spr_master.draw(game.batch);
+		
+		
+		spr_Background.draw(game.batch);
 				
 		game.batch.end();
 		
@@ -100,7 +121,6 @@ public class MetroStation implements Screen, ApplicationListener, InputProcessor
 		Vector3 coordsTouch = camera.unproject(new Vector3(p1,p2,0));
 		
 		if(coordsTouch.x >= 50) {
-			frameState = "WalkRight";
 			return false;
 		}
 		
@@ -110,9 +130,6 @@ public class MetroStation implements Screen, ApplicationListener, InputProcessor
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
 		// TODO Auto-generated method stub
-		
-		frameState = "Stop";
-		
 		return false;
 	}
 
