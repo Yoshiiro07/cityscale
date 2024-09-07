9beb2445362ed3d889868384a9de7601ae069a7d
Gui Oliveira <59020911+Yoshiiro07@users.noreply.github.com>
2024-09-07 00:50:25 -0300

    Update para gdx .14
    Update para gdx .14

--- core/src/main/java/com/moonbolt/cityscale/MainGame.java
+++ core/src/main/java/com/moonbolt/cityscale/MainGame.java
@@ -1,8 +1,15 @@
 package com.moonbolt.cityscale;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.g2d.*;
 
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class MainGame extends Game {
 	
 	private boolean network = false;
